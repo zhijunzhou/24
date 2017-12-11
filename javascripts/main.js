6591a77604962b34cbb993d079b104bcadf29142
Zhou <zhijunzhou@users.noreply.github.com>
2017-12-11 15:11:18 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,23 @@
-console.log('This would be the main JS file.');
 $(function() {
   if (window.DeviceOrientationEvent) {
-    window.addEventListener('deviceorientation', function(e) {
-      // 我们从事件“e”中获取角度值并转化成弧度值。
-      leftRightAngle = parseInt(e.gamma, 10);
-      frontBackAngle = parseInt(e.beta, 10);
-      zAngle = parseInt(e.alpha, 10);
-      if(leftRightAngle || frontBackAngle) {
-        $('#angle_area').html("(x, y, z): (" + frontBackAngle + ", " + leftRightAngle + ", " + zAngle +")");
-      } 
-        if(leftRightAngle>45||leftRightAngle<-45){
-                
-        }
+    window.addEventListener('deviceorientation', function(e) {   
+      var water = document.getElementById('animation');
+      var xAngle = parseInt(e.beta, 10);
+      var yAngle = parseInt(e.gamma, 10);
+      var zAngle = parseInt(e.alpha, 10);
+      
+      if(xAngle || yAngle || zAngle) {
+        $('#angle_area').html("(x, y, z): (" + xAngle + ", " + yAngle + ", " + zAngle +")");
+      }
+      
+      if(water) {
+        water.style.transform = "rotate(-" + xAngle + "deg) scale(2, 1)";
+        water.style.transformOrigin = "top center";
+      }
     }, false);
-  } else if (window.OrientationEvent) { //另一个选项是Mozilla版本同样的东西
-      window.addEventListener('MozOrientation', function(e) {
-        //在这里将长度值当做一个单位，并转换成角度值，看起来运行的不错。
-        leftRightAngle = e.x * Math.PI/2;
-        frontBackAngle = e.y * Math.PI/2;
-      }, false);
+  } else if (window.OrientationEvent) {
+    // todo
   } else {
-      // 自然地，没有浏览器支持的大多数人会获取这个。
-  alert(2222)
+    // todo
   }
 })
